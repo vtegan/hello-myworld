237f88e5c8d408dcfd9f434a07313dc78f2ad936
vtegan <vtegan@users.noreply.github.com>
2017-02-24 10:24:17 -0500

    Added basic class to the branch of the repository.

---
+++ src/org/vegan/basic/MyHelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vegan.basic;
+
+/**
+ * @author egan
+ *
+ */
+public class MyHelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello My World");
+
+	}
+
+}
